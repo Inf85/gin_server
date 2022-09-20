70e753cb4a06c9aa480ab16959468bf10b3ef667
Inf85 <mikhailpopov0585@gmail.com>
2022-09-20 17:52:54 +0300

    first commit

--- server.go
+++ server.go
@@ -1,1 +1,26 @@
 package gin_server
+
+import (
+	"github.com/Inf85/gin_server/helpers"
+	"github.com/Inf85/gin_server/log"
+	"github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
+	"os"
+)
+
+func StartServer() *gin.Engine {
+	_ = godotenv.Load()
+	var err error
+
+	r := gin.Default()
+
+	port := helpers.GetEnvDefault("PORT", "3000")
+	if err = r.Run(":" + port); err != nil {
+		log.GetLogger().Errorf("Can not start server: %s", err.Error())
+		os.Exit(1)
+	}
+
+	return r
+}
